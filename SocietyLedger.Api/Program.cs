7e225f510e663421b4f7e2890705cd974e45b090
Botsa-Santosh <v-santhosh@mvnoc.com>
2026-03-08 15:51:53 +0530

    fix: add missing Microsoft.EntityFrameworkCore using for ExecuteSqlRawAsync

--- SocietyLedger.Api/Program.cs
+++ SocietyLedger.Api/Program.cs
@@ -3,6 +3,7 @@ using Hangfire;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Serilog;
